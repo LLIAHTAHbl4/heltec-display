f4611e015e829ac8286cc0e49f37d62d0abadd2b
LLIAHTAHbl4 <121956202+LLIAHTAHbl4@users.noreply.github.com>
2025-12-29 17:21:16 +0500

    Refactor Heltec V3.1 code for OLED display setup
    Updated the code for Heltec V3.1 to use defined pins for OLED display and added initialization for the reset pin.

--- heltec.ino
+++ heltec.ino
@@ -1,14 +1,27 @@
-// Простой код для Heltec V3.1
+// Простой код для ESP32 с дисплеем SH1106
 #include <Wire.h>
 #include "SH1106Wire.h"
 
-SH1106Wire display(0x3C, 15, 4);
+// Пины для Heltec WiFi Kit 32 V3.1
+#define OLED_SDA 15
+#define OLED_SCL 4
+#define OLED_RST 16
+
+SH1106Wire display(0x3C, OLED_SDA, OLED_SCL);
 
 void setup() {
+  // Настройка пина сброса
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(50);
+  digitalWrite(OLED_RST, HIGH);
+  
+  // Инициализация дисплея
   display.init();
   display.flipScreenVertically();
   display.clear();
   
+  // Вывод текста
   display.setFont(ArialMT_Plain_16);
   display.drawString(0, 0, "Heltec V3.1");
   display.drawString(0, 20, "Привет!");
