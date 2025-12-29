3d5af510f571ac18e6664f22139e026148c277fa
LLIAHTAHbl4 <121956202+LLIAHTAHbl4@users.noreply.github.com>
2025-12-29 19:44:48 +0500

    Switch display initialization from SPI to I2C
    Updated the display initialization to support I2C and added necessary configurations.

--- heltec-display.ino
+++ heltec-display.ino
@@ -1,39 +1,65 @@
-#include <SPI.h>
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-// Пины для SPI дисплея SH1106
-#define OLED_MOSI   13  // SDI
-#define OLED_CLK    14  // SCL
-#define OLED_DC     27  // DC
-#define OLED_CS     12  // CS
-#define OLED_RESET  33  // RES
+// Для I2C дисплея (пробуем еще раз)
+#define I2C_SDA 15
+#define I2C_SCL 4
+#define I2C_RST 16
 
-Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+// Для SPI дисплея (если вдруг SPI)
+#define SPI_MOSI 13
+#define SPI_CLK  14
+#define SPI_DC   27
+#define SPI_CS   12
+#define SPI_RST  33
+
+// Пробуем I2C сначала
+Adafruit_SH1106 display(128, 64, &Wire, I2C_RST);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("Testing SPI display...");
+  Serial.println("Testing display...");
   
-  // Инициализация SPI дисплея
-  if (!display.begin()) {
-    Serial.println("SPI display init failed!");
-    while(1);
+  // Пробуем I2C
+  Serial.println("Trying I2C...");
+  Wire.begin(I2C_SDA, I2C_SCL);
+  
+  // Сброс дисплея
+  pinMode(I2C_RST, OUTPUT);
+  digitalWrite(I2C_RST, LOW);
+  delay(50);
+  digitalWrite(I2C_RST, HIGH);
+  delay(50);
+  
+  // Инициализация I2C дисплея
+  if (display.begin(0x3C, true)) {
+    Serial.println("I2C display OK!");
+    showText("I2C WORKS!");
+  } else {
+    Serial.println("I2C failed, trying 0x3D...");
+    if (display.begin(0x3D, true)) {
+      Serial.println("I2C 0x3D OK!");
+      showText("I2C 0x3D");
+    } else {
+      Serial.println("I2C completely failed");
+    }
   }
-  
+}
+
+void showText(String msg) {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SH110X_WHITE);
-  display.setCursor(0,0);
-  display.println("SPI TEST");
+  display.setCursor(0, 0);
+  display.println(msg);
   display.println("HELLO!");
   display.display();
-  
-  Serial.println("SPI display should work!");
 }
 
 void loop() {
   delay(1000);
+  Serial.println("Running...");
 }
