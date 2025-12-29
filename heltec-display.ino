0aaee298f0f88ee47a7b8612edbb71ad496b6419
LLIAHTAHbl4 <121956202+LLIAHTAHbl4@users.noreply.github.com>
2025-12-29 19:35:56 +0500

    Refactor to use SPI display and remove I2C code
    Removed I2C pin testing and scanning functions, added SPI display initialization and testing.

--- heltec-display.ino
+++ heltec-display.ino
@@ -1,39 +1,39 @@
+#include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+// Пины для SPI дисплея SH1106
+#define OLED_MOSI   13  // SDI
+#define OLED_CLK    14  // SCL
+#define OLED_DC     27  // DC
+#define OLED_CS     12  // CS
+#define OLED_RESET  33  // RES
+
+Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("Testing SPI display...");
   
-  // Пробуем все возможные комбинации пинов
-  int sdaPins[] = {15, 4, 17, 18, 8, 21};
-  int sclPins[] = {4, 15, 18, 17, 9, 22};
-  
-  for (int i = 0; i < 6; i++) {
-    Serial.print("\nTesting SDA:");
-    Serial.print(sdaPins[i]);
-    Serial.print(" SCL:");
-    Serial.println(sclPins[i]);
-    
-    Wire.begin(sdaPins[i], sclPins[i]);
-    delay(100);
-    
-    // Сканируем адреса
-    scanI2C();
+  // Инициализация SPI дисплея
+  if (!display.begin()) {
+    Serial.println("SPI display init failed!");
+    while(1);
   }
+  
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0,0);
+  display.println("SPI TEST");
+  display.println("HELLO!");
+  display.display();
+  
+  Serial.println("SPI display should work!");
 }
 
-void scanI2C() {
-  byte error, address;
-  
-  for(address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    
-    if (error == 0) {
-      Serial.print("Found at 0x");
-      if(address < 16) Serial.print("0");
-      Serial.println(address, HEX);
-    }
-  }
+void loop() {
+  delay(1000);
 }
-
-void loop() {}
